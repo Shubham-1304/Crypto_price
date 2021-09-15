dcdf60584d3a4e472409e2b616d28ad99caccf92
shubh123 <shubh@example.com>
2021-09-15 07:48:11 +0530

    code updated

--- app.py
+++ app.py
@@ -56,6 +56,30 @@ def postMessage():
 @server.route('/')
 def getMessage():
     #app.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    def cwc(msg):
+        curl="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+        params={'symbol':msg,'convert':'INR'}
+        headers={'X-CMC_PRO_API_KEY':os.getenv("token")}
+        req=requests.get(curl,headers=headers,params=params).json()
+        write_json(req)
+        try:
+            price=req['data'][msg]['quote']['INR']['price']
+            return price
+        except:
+            return "No data"
+
+
+    def write_json(r,filename='response_c.json'):
+        with open(filename,'w') as f:
+            json.dump(r,f,indent=4,ensure_ascii=False)
+
+    app=telebot.TeleBot(os.getenv("token2"))
+    @app.message_handler(commands=['start'])
+    def greet(message):
+        app.reply_to(message,f"{message.text}")
+
+
+
     @app.message_handler(func=lambda m:True)
     def crypto(message):
         try:
